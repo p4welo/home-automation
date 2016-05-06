e4deed6a02eb22987231da6c19852e930a90c4fd
Paweł Radomski <pawel.radomski@cohesiva.com>
2016-05-06 16:17:48 +0200

    cosmethic

--- web-gui/protractor.conf.js
+++ web-gui/protractor.conf.js
@@ -4,24 +4,24 @@ var paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
 
 // An example configuration file.
 exports.config = {
-  // The address of a running selenium server.
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
-  //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
+    // The address of a running selenium server.
+    //seleniumAddress: 'http://localhost:4444/wd/hub',
+    //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'chrome'
-  },
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName': 'chrome'
+    },
 
-  baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000',
 
-  // Spec patterns are relative to the current working directory when
-  // protractor is called.
-  specs: [paths.e2e + '/**/*.js'],
+    // Spec patterns are relative to the current working directory when
+    // protractor is called.
+    specs: [paths.e2e + '/**/*.js'],
 
-  // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    }
 };
